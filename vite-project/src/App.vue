f7be0f1a73f5c6c4e15d655af2358a80d115b932
Ghader <gharibyan.ghader@gmail.com>
2023-09-25 01:55:33 +0200

    simple style

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <img src="/vite-deno.svg" alt="Vite with Deno" />
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mt-5">
+    <HelloWorld msg="Vite + Vue" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,14 +1,31 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 defineProps({
   msg: String
 });
 let message = ref('');
-onMounted(async () => {
+let x = ref(0);
+// onMounted(async () => {
+//   const model = tf.sequential();
+//   model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+//   // Prepare the model for training: Specify the loss and the optimizer.
+//   model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+//   // Generate some synthetic data for training. (y = 2x - 1)
+//   const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+//   const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+
+//   // Train the model using the data.
+//   await model.fit(xs, ys, { epochs: 250 });
+//   message = model.predict(tf.tensor2d([20], [1, 1])).dataSync();
+// });
+function predictValues() {
+  console.log('find y');
+  const vals = [];
+  vals.push(x);
   const model = tf.sequential();
   model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
   // Prepare the model for training: Specify the loss and the optimizer.
   model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
@@ -17,16 +34,29 @@ onMounted(async () => {
   const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
   // Train the model using the data.
-  await model.fit(xs, ys, { epochs: 250 });
-  message = model.predict(tf.tensor2d([20], [1, 1])).dataSync();
-});
-
+  model
+    .fit(xs, ys, { epochs: 250 })
+    .then((result) => {
+      message = model.predict(tf.tensor2d(vals, [1, 1])).dataSync();
+      console.log('fittt', message);
+    })
+    .catch((err) => {});
+}
 const count = ref(0);
 </script>
 
 <template>
-  <div class="div">
-    {{ message }}
+  <div>
+    <div class="field has-addons">
+      <div class="control">
+        <input class="input" type="number" v-model="x" />
+      </div>
+      <div class="control">
+        <button class="button" @click="predictValues">Find y</button>
+      </div>
+    </div>
+    <br />
+    <b>{{ message }}</b>
   </div>
 </template>
 
